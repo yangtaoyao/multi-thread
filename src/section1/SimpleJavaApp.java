306c7216934715206bd648fd56efd1e2564293a5
DESKTOP-O2OE0LP\EDZ <yangty@glodon.com>
2019-08-06 09:24:57 +0800

    init

---
+++ src/section1/SimpleJavaApp.java
@@ -0,0 +1,19 @@
+package section1;
+
+/**
+ *@className  SimpleJavaApp
+ * @author  yangty
+ * @date 2019/8/6 9:03
+ */
+/**
+ * 第一章
+ * 1.1 进程、线程、任务
+ * 进程：程序的运行实例，是程序向操作系统申请资源的基本单位
+ * 线程：进程中可独立执行的最小单位
+ *任务：线程所要完成的计算
+ */
+public class SimpleJavaApp {
+    public static void main(String[] args){
+
+    }
+}
